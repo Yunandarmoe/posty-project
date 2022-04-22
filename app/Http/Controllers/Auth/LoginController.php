f3bdde0faf326e57fbadd09616e198f223650dfb
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-22 15:35:15 +0630

    Form request validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StorePostRequest;
 
 class LoginController extends Controller
@@ -21,13 +19,6 @@ class LoginController extends Controller
 
         $validated = $request->validated();
 
-        User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
-        
         auth()->attempt($request->only('email', 'password'));
 
         return redirect()->route('dashboard');
