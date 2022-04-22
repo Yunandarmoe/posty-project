9b5f540921f7b2a4652ddc227cefa03824c2e9e0
YuNandarMoe <scm.yunandarmoe@gmail.com>
2022-04-22 10:35:25 +0630

    Layouts styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,29 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Posty Project</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-  
-  
+<body class="bg-slate-300">
+  <nav class="p-5 bg-white flex justify-between">
+    <ul class="flex justify-between items-center">
+      <li class="p-3">
+        <a href="">Home</a>
+      </li>
+    </ul>
+
+    <ul class="flex justify-between items-center">
+      <li class="p-3">
+        <a href="">Username</a>
+      </li>
+      <li class="p-3">
+        <a href="">Login</a>
+      </li>
+      <li class="p-3">
+        <a href="">Logout</a>
+      </li>
+    </ul>
+  </nav>
+
   @yield('content')
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,6 @@
 
 
 @section('content')
-  Index
+  
 
 @endsection
